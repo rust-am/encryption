bd55a44d879979e5d35e00d7ecab3a5f4a92eed5
rustam023 <r.91@mail.ru>
2020-11-05 13:27:58 +0300

    add sha2

--- main.rb
+++ main.rb
@@ -4,14 +4,24 @@ puts "Введите слово или фразу для шифрования:"
 phrase = STDIN.gets.chomp
 
 sha256 = Digest::SHA256.new
+sha1 = Digest::SHA1.new
+sha1.update phrase
 md5 = Digest::MD5.new
 md5.update phrase
 
 puts "Каким способом зашифровать:"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 
 way = STDIN.gets.to_i
 
 puts "Вот что получилось:"
-puts way == 2 ? (sha256.hexdigest phrase) : (md5.hexdigest)
+puts case way
+     when 1 then
+       md5.hexdigest
+     when 2 then
+       sha1.hexdigest
+     when 3 then
+       sha256.hexdigest phrase
+     end
